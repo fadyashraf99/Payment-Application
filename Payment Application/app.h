5391a5bb33e8a1a49491c676794a91fe1f9cdf57
Fady Ashraf <95493181+fadyashraf99@users.noreply.github.com>
2022-10-14 02:21:19 +0200

    Add files via upload

---
+++ Payment Application/app.h
@@ -0,0 +1,8 @@
+//#pragma once can be used as a header file guard
+#ifndef APP_H_
+#define APP_H_
+#include "server.h"
+
+void appStart(void);
+
+#endif
---
+++ Payment Application/main.c
@@ -0,0 +1,7 @@
+#include "app.h"
+
+int main()
+{
+	appStart();
+	return 0;
+}
